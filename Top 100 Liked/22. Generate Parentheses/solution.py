689a049272fb9eb5782236fce33741142f6972e6
Vlad Perfilev <vladislav.perfilev@idp.zyfra.com>
2025-08-07 15:35:10 +0300

    Add task

---
+++ Top 100 Liked/22. Generate Parentheses/solution.py
@@ -0,0 +1,17 @@
+﻿from typing import List
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+
+        def back_track(current: str, open_count: int, close_count: int):
+            if len(current) == 2 * n:
+                result.append(current)
+                return
+            if open_count < n:
+                back_track(current + '(', open_count + 1, close_count)
+            if close_count < open_count:
+                back_track(current + ')', open_count, close_count + 1)
+
+        back_track('', 0, 0)
+        return result
---
+++ Top 100 Liked/22. Generate Parentheses/test_solution.py
@@ -0,0 +1,18 @@
+﻿import unittest
+from solution import Solution
+
+class TestLetterCombinations(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
+
+    def test_example1(self):
+        n = 3
+        expected = sorted(["((()))","(()())","(())()","()(())","()()()"])
+        result = sorted(self.sol.generateParenthesis(n))
+        self.assertEqual(result, expected)
+
+    def test_example2(self):
+        n = 1
+        expected = ['()']
+        result = self.sol.generateParenthesis(n)
+        self.assertEqual(result, expected)
